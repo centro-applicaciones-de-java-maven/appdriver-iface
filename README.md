2b557de1dff5532eebf126e294a23c2661c0579e
xurpas7 <57973174+xurpas7@users.noreply.github.com>
2024-04-06 17:10:27 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# appdriver-iface
+ 
---
+++ src/main/java/org/guanzon/appdriver/iface/GEntity.java
@@ -0,0 +1,16 @@
+package org.guanzon.appdriver.iface;
+
+import org.json.simple.JSONObject;
+
+public interface GEntity {
+    public String getColumn(int fnValue);
+    public int getColumn(String fsValue);
+    public int getColumnCount();
+    public String getTable();
+    public Object getValue(int fnColumn);
+    public Object getValue(String fsColumn);
+    public JSONObject setValue(int fnColumn, Object foValue);
+    public JSONObject setValue(String fsColumn, Object foValue);
+    
+    public void list();
+}
