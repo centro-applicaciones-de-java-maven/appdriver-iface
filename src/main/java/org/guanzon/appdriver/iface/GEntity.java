941e1a41247120145f5dc36b155440da29c639a6
xurpas7 <michael_cuison07@yahoo.com>
2024-12-11 17:28:49 +0800

    20241011

--- src/main/java/org/guanzon/appdriver/iface/GEntity.java
+++ src/main/java/org/guanzon/appdriver/iface/GEntity.java
@@ -2,112 +2,19 @@ package org.guanzon.appdriver.iface;
 
 import org.json.simple.JSONObject;
 
-/**
- * Standard template for Guanzon Entities.
- *  
- * @author Marlon Sayson
- * @author Michael Cuison
- * @since April 8, 2024
- */
 public interface GEntity {
-    /**
-     * Gets the column index name.
-     * @param fnValue - column index number
-     * @return column index name
-     */
     String getColumn(int fnValue);
-    
-    /**
-     * Gets the column index number.
-     * @param fsValue - column index name
-     * @return column index number
-     */
     int getColumn(String fsValue);
-    
-    /**
-     * Gets the total number of column.
-     * @return total number of column
-     */
     int getColumnCount();
-    
-    /**
-     * Gets the edit mode of the record.
-     * @return edit mode
-     */
     int getEditMode();
-    
-    /**
-     * Gets the table name.
-     * @return table name
-     */
     String getTable();
-    
-    /**
-     * Gets the value of a column index number.
-     * 
-     * @param fnColumn - column index number
-     * @return object value
-     */
     Object getValue(int fnColumn);
-    
-    /**
-     * Gets the value of a column index name.
-     * 
-     * @param fsColumn - column index name
-     * @return object value
-     */
     Object getValue(String fsColumn);
-    
-    /**
-     * Sets column value.
-     * 
-     * @param fnColumn - column index number
-     * @param foValue - value
-     * @return result as success/failed
-     */
     JSONObject setValue(int fnColumn, Object foValue);
-    
-    /**
-     * Sets column value.
-     * 
-     * @param fsColumn - column index name
-     * @param foValue - value
-     * @return result as success/failed
-     */
     JSONObject setValue(String fsColumn, Object foValue);
-    
-    /**
-     * Set the edit mode of the entity to new.
-     * 
-     * @return result as success/failed
-     */
     JSONObject newRecord();
-    
-    /**
-     * Opens a record.
-     * 
-     * @param fsCondition - filter values
-     * @return result as success/failed
-     */
     JSONObject openRecord(String fsCondition);
-    
-    /**
-     * Set the edit mode of the entity to new.
-     * 
-     * @return result as success/failed
-     */
     JSONObject updateRecord();
-    
-    /**
-     * Save the entity.
-     * 
-     * @return result as success/failed
-     */
     JSONObject saveRecord();
-    
-    /**
-     * Prints all the public methods used<br>
-     * and prints the column names of this entity.
-     */
     void list();
 }
--- src/main/java/org/guanzon/appdriver/iface/GRecord.java
+++ src/main/java/org/guanzon/appdriver/iface/GRecord.java
@@ -2,104 +2,18 @@ package org.guanzon.appdriver.iface;
 
 import org.json.simple.JSONObject;
 
-/**
- * Standard template for record entry and modifications.
- *  
- * @author Marlon Sayson
- * @author Michael Cuison
- * @since April 8, 2024
- */
 public interface GRecord {
-    /**
-     * Get the edit mode of the transaction.
-     * 
-     * @return org.guanzon.appdriver.constant.EditMode
-     */
     int getEditMode();
-    
-    /**
-     * Sets the status of the record to search.
-     * 
-     * @param fsValue - org.guanzon.appdriver.constant.RecordStat 
-     */
     void setRecordStatus(String fsValue);
-    
-    /**
-     * Creates new record.
-     * 
-     * @return result as success/error
-     */
     JSONObject newRecord();
-    
-    /**
-     * Loads an existing record.
-     * 
-     * @param fsValue - record primary key value
-     * @return result as success/error
-     */
     JSONObject openRecord(String fsValue);
-    
-    /**
-     * Set the edit mode of the record to update.
-     * 
-     * @return result as success/error
-     */
     JSONObject updateRecord();
-    
-    /**
-     * Saves the record.
-     * 
-     * @return result as success/error
-     */
     JSONObject saveRecord();
-    
-    /**
-     * Deletes a record.
-     * 
-     * @return result as success/error
-     */
     JSONObject deleteRecord();
-    
-    /**
-     * Set record status to inactive.
-     * 
-     * @return result as success/error
-     */
     JSONObject deactivateRecord();
-    
-    /**
-     * Set record status to active.
-     * 
-     * @return result as success/error
-     */
     JSONObject activateRecord();
-    
-    /**
-     * Validates record before saving
-     * 
-     * @return result as success/error
-     */
     JSONObject isEntryOkay();
-    
-    /**
-     * Searches a record.
-     * @param fsValue - search value
-     * @param fbByCode - true if will use primary key to search
-     * @return result as success/error
-     */
     JSONObject searchRecord(String fsValue, boolean fbByCode);
-    
-    /**
-     * Gets the model entity object
-     * 
-     * @return Object
-     */
     Object getModel();
-    
-    /**
-     * Gets the SQL query for browsing record
-     * 
-     * @return String
-     */
     String getSQ_Browse();
 }
